b4bd13b85508e16e3bb3d0fcb6102bad646ec13b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-05-19 21:25:08 -0300

    chore: added miro to all pages; improved dev script

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import App, { Container } from 'next/app'
+import Head from 'next/head'
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Head>
+          <script src="https://miro.com/app/static/rtb.sdk.1.1.js" />
+        </Head>
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
+
+export default MyApp
